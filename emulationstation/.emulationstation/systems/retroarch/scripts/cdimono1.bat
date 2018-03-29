fcaee2fccea0eac16ee802205a8e674cba7d8c00
danyboy666 <dany.ferron@outlook.com>
2018-03-29 18:35:15 -0400

    Update cdimono1.bat
    For anyone trying to setup Philips CD-i on the Windows version of Emulationstation  read this [thread](https://github.com/libretro/mame/issues/64) and create a bat file, call it cdimono1.bat and put it in your systems\retroarch\scripts directory
    
    ```
    echo off
    
    set ROMPATH=%~d0\emulationstation\.emulationstation\roms\cdimono1\
    set arg1=%1
    
    %~d0\emulationstation\.emulationstation\systems\retroarch\retroarch.exe -L %~d0\emulationstation\.emulationstation\systems\retroarch\cores\mame_libretro.dll %ROMPATH%%arg1%.txt
    ```
    
    edit your es_system.cfg like so
    
    ```
    <system>
        <name>cdimono1</name>
        <fullname>Philips CD-I</fullname>
        <path>J:\emulationstation\.emulationstation\systems\retroarch\content\mame\cdimono1</path>
    	<extension>.txt</extension>
    <!--	<command>%HOME%\.emulationstation\systems\retroarch\retroarch.exe -L %HOME%\.emulationstation\systems\retroarch\cores\mame_libretro.dll %ROM%</command>  -->
    	<command>%HOME%\.emulationstation\systems\retroarch\scripts\cdimono1.bat %BASENAME%</command>
        <platform>cdimono1</platform>
        <theme>cdimono1</theme>
    </system>
    ```
    
    use this folder scructure for you cdi chd images : 
    
    **%HOME%\.emulationstation\roms\cdimono1\**
       -> **cdimono1.zip**
       -> **zeldaadv.txt**
       -> **zelda's adventure (1995)(philips)(eu).chd**
    
    Also you need to have the **mame/hash/*.xml**  folder in **retroarch/system/** directory. 
    No other setting swill work. No sub folders work either. Excep a folder structure like : roms/mame/cdimono1/ or roms/content/mame/cdimono1/ .
    
    If you want another roms version ex US check the cdi.xml for the exact version the mame emulator need. Also you need to create a new dummy file txt according to what the cdi.xmI need. I don't think the hash need to be the same but the file need to be in chd format. look for the correct no-intro rom set if you're in doubt or can't convert your bin/cue yourself. Any no-intro set version will do. I found mine relatively easy you the www.
    
    It's important that the chd is named exaclty as the ones mame\hash\cdi.xml. I've tested this multiple ways and it comes down to mame can't translate the rom path properly. If you use the proper windows syntaxe with backslashes though it will work. Ence why this little script. This method and other than naming the dummy file with a different extension nothing will work. Other than that you can press the TAB key to configure your controller to work with cdi/mame games but you keyboard should work right off the bat.

--- emulationstation/.emulationstation/systems/retroarch/scripts/cdimono1.bat
+++ emulationstation/.emulationstation/systems/retroarch/scripts/cdimono1.bat
@@ -1,6 +1,16 @@
-echo off
+@ECHO OFF
 
-set ROMPATH=%~d0\emulationstation\.emulationstation\roms\cdimono1\
-set arg1=%1
+:: We need to set some variables for our rom path. Edit accordingly.
+:: You can edit the folders if needed but usually all es installations are similar. 
+
+SET ROMPATH=%~d0\emulationstation\.emulationstation\roms\cdimono1\
+
+:: Accept the %BASENAME% argument from es_system.cfg command section.
+SET arg1=%1
+
+:: This command doesn't need to be change since it's portable for any version of es. 
+:: %~d0 is simply the system variable to identify the drive letter where the bat file resides.
+:: This is the exact same command use originaly in es_systems.cfg execpt we feed mame_libretro 
+:: core the correct ROMPATH structure it needs to find our cdimono1 driver.
 
 %~d0\emulationstation\.emulationstation\systems\retroarch\retroarch.exe -L %~d0\emulationstation\.emulationstation\systems\retroarch\cores\mame_libretro.dll %ROMPATH%%arg1%.txt
